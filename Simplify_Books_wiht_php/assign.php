5e4b37f8e599573b3e7f046498ef729b63f534bd
June woo Kang <harry020104@naver.com>
2024-11-01 20:24:25 -0500

    .

--- Simplify_Books_wiht_php/assign.php
+++ Simplify_Books_wiht_php/assign.php
@@ -2,21 +2,27 @@
 
 $results = file_get_contents('https://google.com/books.json');
 $books = json_decode($results);
+print_r($books->response->books[0]->title);
+
+$simple = [];
+$simple[] = 4;
+print_r($simple);
 
 $simple_books = [];
 $books_length = count($books->response->books);
+echo $books_length;
 
-// Iterate over all the books to extract title, id, author, and cover image
+// We want to return a simple array of books that includes the title, id, author, and cover image
 for ($i = 0; $i < $books_length; $i++) {
-    $book = $books->response->books[$i];
+    print_r($i);
+	$book =  $books->response->books[$i];
     $simple_books[] = [
         'title' => $book->title,
         'id' => $book->id,
         'author' => $book->author,
-        'cover_image' => $book->cover_image
+        'cover_image' => $book->cover
     ];
 }
 
 print_r($simple_books);
-
 ?>
